709d774fa829405fd42c6864ed9dab3294c1a8c6
daianeklein <daiane.klein22@gmail.com>
2023-10-08 11:14:44 -0300

    functions to create 3d shapes

--- 2-course-creative-programming-j5/sketch.js
+++ 2-course-creative-programming-j5/sketch.js
@@ -2,10 +2,66 @@ function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL)
 };
 
+// function draw(){
+//     background(100);
+//     sphere(100);
+// };
+
+//3D box
+// function draw(){
+//     background(255);
+//     orbitControl(100, 100);
+//     box(100, 200, 50); //width, height, depth 
+// }
+
+
+//3D box - same shape
+// function draw(){
+//     background(255);
+//     orbitControl(100, 100);
+//     box(100); //width, height, depth 
+// }
+
+// 3D Sphere
+// function draw(){
+//     background(255);
+//     orbitControl(100, 100);
+//     sphere(100, 24, 24) //radius, detailX, detailY)
+//     //max is 24
+// }
+
+// 3D cone
+// function draw(){
+//     background(100);
+//     orbitControl(100, 100);
+//     cone(100, 300, 20, 15, true) //radius, height, detailX, detailY, cap
+// }
+
+// 3D cilinder
+// function draw(){
+//     background(150);
+//     orbitControl(100, 100);
+//     cylinder(100, 300, 24, 1, false, true) //radius, height, detailX, detailY, topCap, bottonCap
+// }
+
+// 3D Torus
+// function draw(){
+//     background(150);
+//     orbitControl(100, 100);
+//     torus(150, 50, 24, 16); //radius, tuberadius, detailX, detailY
+// }
+
+// 3D Ellipsoid
+// function draw(){
+//     background(200);
+//     orbitControl(100, 100);
+//     ellipsoid(50, 100, 50, 24, 16); //radiuX, radiusY, radiusZ, detailX, detailY
+// }
+
+// 3D plane
 function draw(){
-    background(100);
-    sphere(100);
-
-    //ellipse(width/2, heigh/2, 100, 100);
-
-};
+    background(200);
+    orbitControl(100, 100);
+    normalMaterial();
+    plane(200, 250);
+}
