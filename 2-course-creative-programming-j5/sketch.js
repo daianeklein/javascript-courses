1c7cf74eba4d45b1341564a7963f5d2e49825581
daianeklein <daiane.klein22@gmail.com>
2023-10-07 23:04:22 -0300

    add 2nd course

---
+++ 2-course-creative-programming-j5/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL)
+};
+
+function draw(){
+    background(100);
+    sphere(100);
+
+    //ellipse(width/2, heigh/2, 100, 100);
+
+};
