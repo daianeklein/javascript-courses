4697876195ff61feeb5bbac41d6cf24a0d457561
daianekoya <daiane@getkoya.ia>
2023-10-07 21:47:50 -0300

    add exercises directory

---
+++ 1-course-javascript-from-zero-to-expert/assignments/assignment01.js
@@ -0,0 +1,10 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = 'brazil';
+let continent = 'south africa';
+let population = '216744397'
+
+console.log(country, continent, population)
