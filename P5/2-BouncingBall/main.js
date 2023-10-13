ca17c810e40aecfbb52edd965d034b30c56aed80
daianeklein <daiane.klein22@gmail.com>
2023-10-12 21:16:16 -0300

    add 3 ball

--- P5/2-BouncingBall/main.js
+++ P5/2-BouncingBall/main.js
@@ -1,4 +1,5 @@
-let ball;
+let ball, newBall, thirdBall;
+
 
 function setup(){
     let canvasWidth = 450;
@@ -6,10 +7,9 @@ function setup(){
 
     createCanvas(canvasWidth, canvasHeigth)
 
-
-    ball = new Ball(200, 200, 3, 4, 20);
-    newBall = new Ball(100, 100, 2, 1.5, 30);
-
+    ball = new Ball(200, 200, 3, 4, 20, 150);
+    newBall = new Ball(100, 100, 2, 1.5, 30, 10);
+    thirdBall = new Ball(150, 130, 7, 8, 5, 200);
 
 }
 
@@ -23,18 +23,26 @@ function draw(){
     newBall.moveBall();
     newBall.bounceBall();
 
+    thirdBall.displayBall();
+    thirdBall.moveBall();
+    thirdBall.bounceBall();
+
 }
 
-function Ball(x, y, xspeed, yspeed, radius){
+function Ball(x, y, xspeed, yspeed, radius, color){
     this.x = x;
     this.y = y;
     this.xspeed = xspeed;
     this.yspeed = yspeed;
     this.radius = radius;
+    this.color = color;
+
 
     //public method to display the ball
     this.displayBall = function(){
+        fill(this.color);
         ellipse(this.x, this.y, this.radius * 2);
+
     }
 
     this.moveBall = function(){
