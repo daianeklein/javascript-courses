a24179629926559148c657155e4bcae966e90011
daianeklein <daiane.klein22@gmail.com>
2023-10-31 14:23:33 -0300

    fixed the canvas issue

--- 5-P5-sound/sketch.js
+++ 5-P5-sound/sketch.js
@@ -1,51 +1,75 @@
-let song;
-let slider;
-let sliderVolume;
-let sliderRate;
-let sliderRateLabel;
-let button;
-
-//ampliture (volume)
-let amp;
-let ampHistory = []
-
-
-function preload(){
-    
-}
-
-function playSong(){
-  if (song.isPlaying()) {
-    song.pause();
-  } else {
-    song.play();
+function topRightSketch(p) {
+  p.setup = function() {
+    let canvas = p.createCanvas(200, 200); 
+    canvas.parent('topRight');
   }
 
+  p.draw = function() {
+    p.background(220);
+    p.fill(255, 0, 0);
+    p.ellipse(p.width / 2, p.height / 2, 50, 50); 
+  }
 }
 
-function setup() {
-  canvasContainerTopRight = select('#topRight');
-  let topRightCanvas = createCanvas(canvasContainerTopRight.size().width, canvasContainerTopRight.size().height);
-  topRightCanvas.parent('topRight');
+function topLeftSketch(p) {
+  let button;
+  let song;
 
-    song = loadSound("assets/songs/eua-miley-cyrus.mp3", loaded);
+  p.preload = function() {
+    song = p.loadSound("assets/songs/eua-miley-cyrus.mp3", loaded);
+  }
 
-    button = createButton('Play');
+  p.setup = function() {
+    button = p.createButton('Play');
     button.mousePressed(playSong);
+    button.parent('topLeft');
+  }
 
+  function playSong() {
+    if (song.isPlaying()) {
+      song.pause();
+    } else {
+      song.play();
+    }
+  }
+
+  function loaded() {
+    if (song) {
+      console.log('music loaded!');
+    }
+  }
+}
+
+function bottomRightSketch(p) {
+  let amp;
+  let ampHistory = [];
+
+  p.setup = function() {
+    let canvasContainer = p.select('#bottomRight');
+    let canvas = p.createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+    canvas.parent('bottomRight');
     amp = new p5.Amplitude();
+  }
+
+  p.draw = function() {
+    p.background(220);
+    let vol = amp.getLevel();
+    ampHistory.push(vol);
+    p.stroke(255);
+    p.noFill();
+    p.beginShape();
+    for (let i = 0; i < ampHistory.length; i++) {
+      let y = p.map(ampHistory[i], 0, 1, p.height, 0);
+      p.vertex(i, y);
+    }
+    p.endShape();
+
+    if (ampHistory.length > p.width) {
+      ampHistory.splice(0, 1);
+    }
+  }
 }
 
-function draw() {
-  background(220); // Clear background
-
-  var vol = amp.getLevel();
-  ellipse(width/2, height/2, 100, vol * 1000)
-}
-
-function loaded() {
-  if (song){
-    console.log('music loaded!');
-}
-
-}
+let topRight = new p5(topRightSketch);
+let topLeft = new p5(topLeftSketch);
+let bottomRight = new p5(bottomRightSketch);
