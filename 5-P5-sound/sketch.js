f70f89b5b148ff26575b0b7e75d96a1ed14f9e7c
daianeklein <daiane.klein22@gmail.com>
2023-10-31 12:57:05 -0300

    adjust canvas

--- 5-P5-sound/sketch.js
+++ 5-P5-sound/sketch.js
@@ -5,82 +5,47 @@ let sliderRate;
 let sliderRateLabel;
 let button;
 
+//ampliture (volume)
+let amp;
+let ampHistory = []
+
 
 function preload(){
     
 }
 
+function playSong(){
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+
+}
+
 function setup() {
+  canvasContainerTopRight = select('#topRight');
+  let topRightCanvas = createCanvas(canvasContainerTopRight.size().width, canvasContainerTopRight.size().height);
+  topRightCanvas.parent('topRight');
+
     song = loadSound("assets/songs/eua-miley-cyrus.mp3", loaded);
 
-    if (song){
-        console.log('music loaded!');
-    }
+    button = createButton('Play');
+    button.mousePressed(playSong);
+
+    amp = new p5.Amplitude();
+}
+
+function draw() {
+  background(220); // Clear background
+
+  var vol = amp.getLevel();
+  ellipse(width/2, height/2, 100, vol * 1000)
 }
 
 function loaded() {
-    song.play();
+  if (song){
+    console.log('music loaded!');
 }
 
-function mousePressed() {
-    if (song.isPlaying()) {
-      song.stop();
-    } else {
-      song.play();
-    }
-  }
-
-  // function positionElements() {
-//     // Adjust the positions based on canvas dimensions
-//     slider.position(10, height - 140);
-//     sliderRate.position(10, height - 110);
-//     sliderRateLabel.position(10, height - 85);
-//     button.position(10, height - 60);
-// }
-
-// function windowResized() {
-//     resizeCanvas(windowWidth, windowHeight);
-//     positionElements();
-// }
-
-//     sliderVolume = createSlider(0, 1, 0.5, 0.01); //values between 0 and 1
-//     //slider.position(10, 210); // Position slider inside the bottom div
-//     sliderVolume.id('sound-slider');
-//     sliderVolume.input(changeVolume);  // Attach input event to slider to adjust volume
-    
-//     sliderRate = createSlider(0, 5, 1, 0.01); //speed of the song
-//     sliderRate.id('rate-slider');
-//     sliderRate.input(changeRate);  // Attach input event to slider to adjust playback rate
-
-//     // Create a paragraph element below the sliderRate to display its value
-//     sliderRateLabel = createP("Speed: " + sliderRate.value());
-//     sliderRateLabel.position(10, 265); 
-
-//     button = createButton('Play');
-//     button.id('playButton');
-//     button.parent('play-bottom');
-//     button.mousePressed(togglePlaying);
-
-//     positionElements();
-// }
-
-
-
-// function togglePlaying() {
-//     if (!song.isPlaying()) {
-//         song.play();
-//         button.html('Stop');
-//     } else {
-//         song.stop();
-//         button.html('Play');
-//     }
-// }
-
-// function changeVolume() {
-//     song.setVolume(sliderVolume.value());
-// }
-
-// function changeRate() {
-//     song.rate(sliderRate.value());
-//     sliderRateLabel.html("Speed: " + sliderRate.value().toFixed(2))
-// }
+}
